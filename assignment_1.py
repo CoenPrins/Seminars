8861935fe1156e13eeef8828dad438a7c2efb1e9
Iris Reitsma <irisreitsma@online.nl>
2020-09-24 15:56:18 +0200

    funcion_fixed

--- assignment_1.py
+++ assignment_1.py
@@ -16,12 +16,12 @@ def lower_case(string):
     """
 
     ### your code starts here
-
+    lower_string = string.lower()
     ### your code ends here
 
     return lower_string
 
-
+print(lower_case('HOI'))
 """
 Do the same thing again with upper_case, but change the order so everyone
 experiences at least 1 merge conflict.
@@ -34,7 +34,7 @@ def upper_case(string):
 
     Returns:
     upper_string -- upper case version of string
-    
+
     """
 
     ### your code starts here
